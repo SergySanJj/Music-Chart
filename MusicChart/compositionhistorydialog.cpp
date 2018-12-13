a0ab454df509e5323e5d4c574c15858a245b26a6
Sergei Yarema <isara.isara8@gmail.com>
2018-12-14 01:47:28 +0200

    Style plot

--- MusicChart/compositionhistorydialog.cpp
+++ MusicChart/compositionhistorydialog.cpp
@@ -18,6 +18,9 @@ compositionHistoryDialog::compositionHistoryDialog(Chart &_chart, QDate _date, s
     ui->HistoryPlot->addGraph();
     ui->HistoryPlot->graph(0)->setData(coords.first, coords.second);
 
+    ui->HistoryPlot->graph(0)->setBrush(QBrush(QColor(200,200,200,20)));
+    ui->HistoryPlot->graph(0)->setPen(QPen(QColor(253, 166, 116),3));
+
     ui->HistoryPlot->xAxis->setLabel("months after release");
     ui->HistoryPlot->yAxis->setLabel("popularity");
 
@@ -25,6 +28,18 @@ compositionHistoryDialog::compositionHistoryDialog(Chart &_chart, QDate _date, s
 
     ui->HistoryPlot->xAxis->setRange(0, maxX);
     ui->HistoryPlot->yAxis->setRange(0, 200);
+
+    ui->HistoryPlot->setBackground(QBrush(QColor(120,120,120)));
+
+    ui->HistoryPlot->xAxis->setBasePen(QPen(Qt::white, 1));
+    ui->HistoryPlot->yAxis->setBasePen(QPen(Qt::white, 1));
+
+    ui->HistoryPlot->xAxis->setTickLabelColor(QColor(240,240,240));
+    ui->HistoryPlot->yAxis->setTickLabelColor(QColor(240,240,240));
+
+    ui->HistoryPlot->xAxis->setLabelColor(QColor(250,250,250));
+    ui->HistoryPlot->yAxis->setLabelColor(QColor(250,250,250));
+
     ui->HistoryPlot->replot();
 }
 
